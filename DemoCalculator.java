dac52f1e45a4ae0a6e916a112dbffc8a39fb1668
vishnu4dev <vishnu@MacBookPro.lan>
2025-02-09 15:45:11 +0000

    feat: Method overloading with calc

--- DemoCalculator.java
+++ DemoCalculator.java
@@ -4,12 +4,27 @@ public class DemoCalculator {
     public static void main(String args[]){
         Calculator Calc = new Calculator();
         int res = Calc.add(4,3);
-        System.out.println("Sum of 4 + 3 :"+res);
+        int res1 = Calc.add(4,3,2);
+        int res2 = Calc.add(4);
+        System.out.println("Sum of 4 + 3 : "+res);
+        System.out.println("Sum of 4 + 3 +2 : "+res1);
+        System.out.println("Sum of 4 + 1: "+res2);
+
     }
 }
 
 class Calculator {
     public int add(int a,int b){
      return a + b; 
+    }
+    
+    //Method overloading 
+    public int add (int a, int b, int c){
+        return a + b + c;
+    }
+
+    // Method overloading 
+    public int add (int a){
+        return a + a;
     }
 }
